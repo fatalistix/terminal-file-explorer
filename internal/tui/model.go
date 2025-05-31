966bf8a1369965b7aa2532dec1fe6bec66bc81a7
Viacheslav Balashov <slavabal11@outlook.com>
2025-06-01 00:49:32 +0700

    messaging with model

--- internal/tui/model.go
+++ internal/tui/model.go
@@ -1,65 +1,125 @@
 package tui
 
 import (
-	"fmt"
-	"log"
-
 	tea "github.com/charmbracelet/bubbletea"
-
+	"github.com/charmbracelet/lipgloss"
 	"github.com/fatalistix/terminal-file-explorer/internal/model"
+	"github.com/fatalistix/terminal-file-explorer/internal/service"
+	"github.com/fatalistix/terminal-file-explorer/internal/tui/directory"
 )
 
 type DirectoryLoader interface {
-	LoadDirectory(string) (model.Directory, error)
+	LoadDirectoryContent(string) (model.Directory, error)
+	LoadDirectoryName() (string, error)
 }
 
-type Model struct {
-	DirectoryLoader DirectoryLoader
-	PreviousDir     model.Directory
-	CurrentDir      model.Directory
-	SelectedDir     model.Directory
+type RootModel struct {
+	width, height int
+	stateHolder   *service.StateHolder
 }
 
-func NewModel(DirectoryLoader DirectoryLoader) Model {
-	return Model{
-		DirectoryLoader: DirectoryLoader,
-	}
+func NewRootModel(stateHolder *service.StateHolder) (*RootModel, error) {
+	const op = "tui.NewModel"
+
+	return &RootModel{
+		width:       0,
+		height:      0,
+		stateHolder: stateHolder,
+	}, nil
 }
 
-func (m Model) Init() tea.Cmd {
-	return func() tea.Msg {
-		const op = "tui.Model.Init"
-
-		var err error
-
-		m.CurrentDir, err = m.DirectoryLoader.LoadDirectory(".")
-		if err != nil {
-			return fmt.Errorf("%s: failed to load directory: %w", op, err)
-		}
-
-		return nil
-	}
+func (m *RootModel) Init() tea.Cmd {
+	return nil
 }
 
-func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m *RootModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		{
+			return m.handleWindowSizeMsg(msg)
+		}
 	case tea.KeyMsg:
 		{
-			return m, m.handleKeyMsg(msg)
+			return m.handleKeyMsg(msg)
 		}
 	}
 	return m, nil
 }
 
-func (m Model) View() string {
-    s :=
+func (m *RootModel) handleWindowSizeMsg(msg tea.WindowSizeMsg) (tea.Model, tea.Cmd) {
+	m.width = msg.Width
+	m.height = msg.Height
+
+	return m, nil
 }
 
-func (m Model) handleKeyMsg(msg tea.KeyMsg) tea.Cmd {
+func (m *RootModel) handleKeyMsg(msg tea.KeyMsg) (tea.Model, tea.Cmd) {
 	switch msg.String() {
 	case "ctrl+c", "q":
-		return tea.Quit
+		return m, tea.Quit
+	case "up", "k":
+		m.stateHolder.MoveUp()
+		return m, nil
+	case "down", "j":
+		m.stateHolder.MoveDown()
+		return m, nil
+	case "enter", " ", "l":
+		m.stateHolder.Select()
+		return m, nil
+	case "backspace", "h":
+		m.stateHolder.GoToPreviousDirectory()
+		return m, nil
+	default:
+		return m, nil
+	}
+}
+
+func (m *RootModel) View() string {
+	state := m.stateHolder.GetState()
+
+	averageWidth := m.width / 3
+	remainingWidth := m.width - averageWidth*3
+
+	//m.prevDirView.SetHeight(m.height)
+	//m.prevDirView.SetWidth(averageWidth)
+	//
+	//m.currDirView.SetHeight(m.height)
+	//m.currDirView.SetWidth(averageWidth + remainingWidth)
+
+	prevDirBlock := lipgloss.NewStyle().
+		Height(m.height).
+		Width(averageWidth)
+
+	currDirBlock := lipgloss.NewStyle().
+		Height(m.height).
+		Width(averageWidth + remainingWidth)
+
+	return lipgloss.JoinHorizontal(
+		lipgloss.Top,
+		prevDirBlock.Render(renderView(state.PrevViewContent, averageWidth, m.height)),
+		currDirBlock.Render(renderView(state.CurrViewContent, averageWidth+remainingWidth, m.height)),
+	)
+}
+
+func renderView(content model.Content, width, height int) string {
+	switch content := content.(type) {
+	case *model.EmptyContent:
+		return ""
+	case *model.DirectoryContent:
+		if content.SelectedId < 0 {
+			view := directory.NewView(content.Directory)
+			view.SetWidth(width)
+			view.SetHeight(height)
+			return view.View()
+		} else {
+			selectable := directory.NewSelectable(content.Directory, content.SelectedId)
+			selectable.SetWidth(width)
+			selectable.SetHeight(height)
+			return selectable.View()
+		}
+	case *model.TextContent:
+		return content.Text
 	}
 
-	return nil
+	panic("unreachable")
 }
